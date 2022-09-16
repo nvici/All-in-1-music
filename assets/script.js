2e7a2b84781925393f920b25c39ff70a50a6ab3a
nvici <nvicinanza@outlook.com>
2022-09-15 19:19:10 -0500

    edited css and html and added working  js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+var searchBtn = document.getElementById('search-button');
+
+searchBtn.addEventListener('click',function(e){
+    e.preventDefault()
+    var appearEl = document.querySelector('.parent');
+    appearEl.classList.remove('hide');
+
+    
+})
